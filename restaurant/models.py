ed822df0fde7376513a7d2a98e6b242ae1eb32de
Mikhail Chesnokov <ms.chesnokov@svo.aero>
2024-03-24 17:17:26 +0300

    database and model migration

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here.'
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True, max_length=11)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=0, max_length=6)
+    booking_date = models.DateTimeField()
+
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True, max_length=5)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=0, max_length=5)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import django.views
 
 # Create your views here.
 def index(request):
